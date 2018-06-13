703101cd0eaa942a29d2c7244aa521d1bb8f3331
Paulo Andrino <pauloandrino@yahoo.com.br>
2018-06-13 20:24:49 -0300

    Pesquisa e paginação de lançamento.

--- algamoney-api/src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,12 @@
 package com.example.algamoney.api.repository.lancamento;
 
-import java.util.List;
-
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.filter.LancamentoFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	
 }
